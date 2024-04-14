30f8465f068d2d4818057c74ce0c41054c5be2b8
Ilya <zadonskyilya@gmail.com>
2024-04-14 21:57:56 +0700

    Fix spawn and attack summon

--- modules/summon/summon.gd
+++ modules/summon/summon.gd
@@ -13,28 +13,35 @@ var current_state: PlantStates:
 @export var _attack_power: float
 
 
+var attack_area: Area2D 
 var current_target: DamageableArea2D
 
 
 func _ready() -> void:
 	dead.connect(on_dead)
 	animation_player = $AnimationPlayer
+	attack_area = $AttackArea
+	
+	
+func change_attack_target(target: DamageableArea2D):
+	current_state = PlantStates.ATTACK
+	current_target = target
 
 
 func on_dead():
 	_summon_signals.summon_dead.emit(_type, global_position)
-	current_state = PlantStates.Death
+	current_state = PlantStates.DEATH
 
 
 func play_anim_with_state(state: PlantStates):
 	match state:
-		PlantStates.Idle:
+		PlantStates.IDLE:
 			animation_player.play("idle")
-		PlantStates.Spawn:
+		PlantStates.SPAWN:
 			animation_player.play("spawn")
-		PlantStates.Attack:
+		PlantStates.ATTACK:
 			animation_player.play("attack")
-		PlantStates.Death:
+		PlantStates.DEATH:
 			animation_player.play("death")
 			await animation_player.animation_finished
 			#надо заменить удаление логикогой прехеода в собираемый объект
@@ -52,9 +59,28 @@ func attack():
 	
 	
 func _on_attack_area_entered(area: Area2D) -> void:
+	if current_state == PlantStates.SPAWN:
+		return
+	
 	if area is DamageableArea2D:
 		current_target = area
-		current_state = PlantStates.Attack
+		current_state = PlantStates.ATTACK
+		
+		
+func check_attack_area():
+	for overlapping_area in attack_area.get_overlapping_areas():
+		if overlapping_area == current_target:
+			continue
+	
+		if overlapping_area is DamageableArea2D:
+			change_attack_target(overlapping_area)
+			return
+			
+	current_state = PlantStates.IDLE
+	
+	
+func _on_attack_area_exited(area: Area2D) -> void:
+	check_attack_area()
 
 
 func _input(event: InputEvent) -> void:
@@ -69,8 +95,8 @@ func _input(event: InputEvent) -> void:
 
 enum PlantStates
 {
-	Spawn = 0,
-	Idle = 1,
-	Attack = 2,
-	Death = 3
+	SPAWN = 0,
+	IDLE = 1,
+	ATTACK = 2,
+	DEATH = 3
 }
