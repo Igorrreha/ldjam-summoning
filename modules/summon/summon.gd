118678eff7c765e55b48bbd1c0a0416894804c5c
Ilya <zadonskyilya@gmail.com>
2024-04-14 18:26:30 +0700

    Update graphic for tree and summon, summon anim

--- modules/summon/summon.gd
+++ modules/summon/summon.gd
@@ -1,9 +1,39 @@
 extends DamageableArea2D
 
+var animation_player: AnimationPlayer
+var current_state: PlantStates:
+	set(v):
+		current_state = v
+		play_anim_with_state(v)
 
 func _ready() -> void:
 	dead.connect(on_dead)
+	animation_player = $AnimationPlayer
 
 
 func on_dead():
-	queue_free()
+	current_state = PlantStates.Death
+
+
+func play_anim_with_state(state: PlantStates):
+	match state:
+		PlantStates.Idle:
+			animation_player.play("idle")
+		PlantStates.Spawn:
+			animation_player.play("spawn")
+		PlantStates.Attack:
+			animation_player.play("attack")
+		PlantStates.Death:
+			animation_player.play("death")
+			await animation_player.animation_finished
+			#надо заменить удаление логикогой прехеода в собираемый объект
+			queue_free()
+
+
+enum PlantStates
+{
+	Spawn = 0,
+	Idle = 1,
+	Attack = 2,
+	Death = 3
+}
