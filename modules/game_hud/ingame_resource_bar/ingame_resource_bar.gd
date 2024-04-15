41368d460f548e6d3224d418aa6203f65b81a395
Igorrreha <igorecha.9999@gmail.com>
2024-04-16 03:11:27 +0700

    Update resources bar visuals

--- modules/game_hud/ingame_resource_bar/ingame_resource_bar.gd
+++ modules/game_hud/ingame_resource_bar/ingame_resource_bar.gd
@@ -2,6 +2,7 @@ extends ProgressBar
 
 
 @export var _ingame_resource: IngameResource
+@onready var _label: Label = $Label
 
 
 func _ready() -> void:
@@ -13,3 +14,4 @@ func _update_state() -> void:
 	value = _ingame_resource.value
 	min_value = _ingame_resource.min_value
 	max_value = _ingame_resource.max_value
+	_label.text = "%s/%s" % [floori(value), floori(max_value)]
