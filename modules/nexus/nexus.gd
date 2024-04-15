476a2ca7e99185b776a41a0bb118857debfd3361
DESKTOP-4GE1PFI\zadon <zadonskiyilya@gmail.com>
2024-04-16 04:57:01 +0700

    Loose game sound! (very cool)

--- modules/nexus/nexus.gd
+++ modules/nexus/nexus.gd
@@ -3,9 +3,13 @@ extends DamageableArea2D
 
 
 @export var _game_state_signals: GameStateSignals
+@export var audio_player: AudioStreamPlayer2D
+@export var loose_game_sound: AudioStream
 
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_EXIT_TREE\
 	and _health <= 0:
 		_game_state_signals.game_over.emit()
+		audio_player.stream = loose_game_sound
+		audio_player.play()
