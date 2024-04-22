c7ff594b77ddb3e720766a078f9445d76c28ec86
Igorrreha <igorecha.9999@gmail.com>
2024-04-22 10:10:20 +0700

    Make modules graph more stylish

--- addons/modules_graph/module_node/module_graph_node.gd
+++ addons/modules_graph/module_node/module_graph_node.gd
@@ -7,6 +7,12 @@ signal double_clicked
 
 
 @export var _module_node_graphs_signal: ModuleNodesSignals
+@export var _title_label_settings: LabelSettings
+
+
+func _ready() -> void:
+	for child: Label in get_titlebar_hbox().get_children():
+		child.label_settings = _title_label_settings
 
 
 func _gui_input(event: InputEvent) -> void:
