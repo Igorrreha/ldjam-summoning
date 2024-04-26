a547ef53e8d08d29ac65c8b94b8e84fda2e5815b
Igorrreha <igorecha.9999@gmail.com>
2024-04-27 00:36:19 +0700

    Minor fixes

--- addons/modules_map/plugin.gd
+++ addons/modules_map/plugin.gd
@@ -3,11 +3,17 @@ extends EditorPlugin
 
 
 const PLUGIN_SCENE: PackedScene = preload("res://addons/modules_map/plugin.tscn")
+var _editor_base_color_style_box: StyleBoxFlat =\
+	preload("res://addons/modules_map/editor_base_color_style_box.tres")
 var _plugin_node: Control
 var _container_type = EditorPlugin.CONTAINER_CANVAS_EDITOR_MENU
 
 
 func _enter_tree() -> void:
+	var editor_settings = get_editor_interface().get_editor_settings()
+	var base_color = editor_settings.get_setting("interface/theme/base_color")
+	_editor_base_color_style_box.bg_color = base_color
+	
 	_plugin_node = PLUGIN_SCENE.instantiate() as Control
 	EditorInterface.get_editor_main_screen().add_child(_plugin_node)
 	_make_visible(false)
