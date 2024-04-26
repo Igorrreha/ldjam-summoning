1317e23790292ca71d809f6e5827bdabe895cc4f
Igorrreha <igorecha.9999@gmail.com>
2024-04-26 22:19:51 +0700

    Extract toggleable split container script from scene

---
+++ addons/modules_map/toggleable_split_container/toggleable_split_container.gd
@@ -0,0 +1,22 @@
+@tool
+extends HSplitContainer
+
+
+@export var _toggle_button: CheckButton
+
+var _is_active: bool
+
+
+func _ready() -> void:
+	_is_active = not "CanvasItemEditor" in str(get_path())
+	
+	if _is_active:
+		_toggle_button.toggled.connect(_on_toggled)
+
+
+func _on_toggled(value: bool) -> void:
+	collapsed = value
+	if collapsed:
+		dragger_visibility = SplitContainer.DRAGGER_HIDDEN_COLLAPSED
+	else:
+		dragger_visibility = SplitContainer.DRAGGER_VISIBLE
